b96b489df21fc9c4fe76a61448f0fdfae788b76d
pro0518 <justmusic0518@gmail.com>
2026-05-11 20:39:52 +0900

    [조범근] [UI 이미지 개선] Lobby에서 현재 누른 창의 버튼이 활성화 되어 있도록 설정

--- Source/Team21_CH3_Project/OutGameUI/Widget/UOutGameCommonHeaderWidget.cpp
+++ Source/Team21_CH3_Project/OutGameUI/Widget/UOutGameCommonHeaderWidget.cpp
@@ -1,6 +1,7 @@
 ﻿// UOutGameCommonHeaderWidget.cpp
 #include "UOutGameCommonHeaderWidget.h"
 #include "Components/Button.h"
+#include "Components/Image.h"
 #include "OutGameUI/Controller/OutGamePlayerController.h"
 #include "OutGameUI/Widget/OutGameRootWidget.h"
 
@@ -9,6 +10,34 @@ void UUOutGameCommonHeaderWidget::NativeOnInitialized(){
 	if (IsValid(weaponsButton) == true) weaponsButton->OnClicked.AddUniqueDynamic(this, &ThisClass::HandleWeaponsClicked);
 	if (IsValid(storeButton) == true) storeButton->OnClicked.AddUniqueDynamic(this, &ThisClass::HandleStoreButton);
 	if (IsValid(settingsButton) == true) settingsButton->OnClicked.AddUniqueDynamic(this, &ThisClass::HandleSettingsClicked);
+}
+
+void UUOutGameCommonHeaderWidget::SetActiveTab(EOutGameWidgetType widgetType){
+	//SetButtonSelected(playButton, widgetType == EOutGameWidgetType::MainMenu);
+	//SetButtonSelected(settingsButton, widgetType == EOutGameWidgetType::Settings);
+	//SetButtonSelected(storeButton, widgetType == EOutGameWidgetType::Store);
+	//SetButtonSelected(weaponsButton, widgetType == EOutGameWidgetType::WeaponPreview);
+
+	
+	SetSelectedVisible(playSelectedImage, widgetType == EOutGameWidgetType::MainMenu);
+	SetSelectedVisible(weaponsSelectedImage, widgetType == EOutGameWidgetType::WeaponPreview);
+	SetSelectedVisible(storeSelectedImage, widgetType == EOutGameWidgetType::Store);
+	SetSelectedVisible(settingsSelectedImage, widgetType == EOutGameWidgetType::Settings);
+}
+
+void UUOutGameCommonHeaderWidget::SetSelectedVisible(UImage* image, bool bSelected){
+	/*
+	if (IsValid(button) == false) return;
+	
+	button->SetBackgroundColor(
+		bSelected
+			? FLinearColor(1.0f, 1.0f, 1.0f, 1.0f)
+			: FLinearColor(0.45f, 0.45f, 0.45f, 1.0f)
+	);
+	*/
+	if (IsValid(image) == false) return;
+
+	image->SetVisibility(bSelected ? ESlateVisibility::HitTestInvisible : ESlateVisibility::Collapsed);
 }
 
 void UUOutGameCommonHeaderWidget::HandlePlayClicked(){
