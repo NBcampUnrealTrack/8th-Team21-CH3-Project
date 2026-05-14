eadfce19cd26338eeac066f68473333d14b940a2
pro0518 <justmusic0518@gmail.com>
2026-05-14 20:09:30 +0900

    [조범근][NewGame 클릭시 Gold초기화]

--- Source/Team21_CH3_Project/OutGameUI/Widget/OutGameRootWidget.cpp
+++ Source/Team21_CH3_Project/OutGameUI/Widget/OutGameRootWidget.cpp
@@ -74,6 +74,7 @@ void UOutGameRootWidget::ShowLobby(){
 	{
 		if (UOutGameMainMenuWidget* MainMenuInstance = Cast<UOutGameMainMenuWidget>(ScreenSwitcher->GetActiveWidget()))
 		{
+			
 			MainMenuInstance->ShowLobby();
 		}
 	}
@@ -268,7 +269,7 @@ void UOutGameRootWidget::HandleConfirmAccepted(){
 	case EConfirmAction::NewGame:
 		if (UTeamGameInstance* GI = Cast<UTeamGameInstance>(GetWorld()->GetGameInstance()))
 			GI->StartNewGame();
-		
+		if (IsValid(commonHeaderWidget) == true) commonHeaderWidget->UpdateCommonUI();
 		ShowTransition([this]
 		{
 			ShowLobby();
