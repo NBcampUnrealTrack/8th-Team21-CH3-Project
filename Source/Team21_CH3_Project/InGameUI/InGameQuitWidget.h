1121605ed4efcbc6dd32fd17ab147333e35fd953
pro0518 <justmusic0518@gmail.com>
2026-05-20 15:22:17 +0900

    [조범근][InGameQuitWidget 최초 생성]

---
+++ Source/Team21_CH3_Project/InGameUI/InGameQuitWidget.h
@@ -0,0 +1,18 @@
+﻿// InGameQuitWidget.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InGameQuitWidget.generated.h"
+
+
+UCLASS()
+class TEAM21_CH3_PROJECT_API UInGameQuitWidget : public UUserWidget{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeOnInitialized() override;	
+	
+	UFUNCTION()
+	void HandleBackRequested();
+};
